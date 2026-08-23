2073550008a923d20b37e3ba0f2d7148a6a72002
Chang Xu <cxu.taobao@gmail.com>
2026-08-23 04:45:14 -0500

    added get_photos mock function in client to access list_photos in server

--- client/sync.py
+++ client/sync.py
@@ -3,7 +3,7 @@ import httpx
 SERVER_URL = "http://127.0.0.1:8000"
 
 
-def get_photos():
+def get_photos_names():
     response = httpx.get(f"{SERVER_URL}/photos")
     response.raise_for_status()
 
@@ -11,5 +11,5 @@ def get_photos():
 
 
 if __name__ == "__main__":
-    photos = get_photos()
+    photos = get_photos_names()
     print(photos)
