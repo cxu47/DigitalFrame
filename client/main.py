8dfe0d33bbb0c515b2f40769110d2df53aa07f4a
Chang Xu <cxu.taobao@gmail.com>
2026-08-23 05:58:53 -0500

    added basic client main with sync and then slideshow functions

---
+++ client/main.py
@@ -0,0 +1,11 @@
+from client.sync import sync_photos
+from client.slideshow import show_slideshow
+
+
+def main():
+    sync_photos()
+    show_slideshow()
+
+
+if __name__ == "__main__":
+    main()
