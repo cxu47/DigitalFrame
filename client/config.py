873c7530872acc9b854319df861ddd07a1c7baaf
Chang Xu <cxu.taobao@gmail.com>
2026-08-24 20:28:17 -0500

    Add SYNC_INTERVAL configuration to client config

--- client/config.py
+++ client/config.py
@@ -4,6 +4,7 @@ CLIENT_DIR = Path(__file__).resolve().parent
 CACHE_DIR = CLIENT_DIR / "cache"
 SERVER_URL = "http://127.0.0.1:8000"
 TIMEOUT = 60
+SYNC_INTERVAL = 60
 DISPLAY_SECONDS = 5
 IDLE_SECONDS = 0.5
 
