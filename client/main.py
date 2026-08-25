7657882968c21f87d2d2c5a609de3607c4f5ae9f
Chang Xu <cxu.taobao@gmail.com>
2026-08-24 20:56:18 -0500

    updated main

--- client/main.py
+++ client/main.py
@@ -6,8 +6,22 @@ from .slideshow import show_slideshow
 from .config import SYNC_INTERVAL
 
 
+def sync_loop():
+    while True:
+        time.sleep(SYNC_INTERVAL)
+        sync_photos()
+
+
 def main():
     sync_photos()
+
+    sync_thread = threading.Thread(
+        target=sync_loop,
+        daemon=True,
+    )
+    sync_thread.start()
+
+    show_slideshow()
 
 
 if __name__ == "__main__":
